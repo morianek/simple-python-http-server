a2f635a379bd65c9583724a327aee4a27ad4459c
marcin kossakowski <morianekxd@gmail.com>
2025-02-15 19:57:40 +0100

    added internal error site

--- config/response_config.py
+++ config/response_config.py
@@ -3,12 +3,14 @@ from dataclasses import dataclass
 PATH_TO_HTTP = "html/"
 DEFAULT_FILES = ["index.html", "lol.html"]
 NOT_FOUND_FILE = "error_404.html"
+INTERNAL_ERROR_FILE = "error_500.html"
 
 @dataclass
 class ResponseConfig:
     path_to_http_dir: str
     default_files: list[str]
     not_found_file: str
+    internal_error_file: str
 
 def get_config():
-    return ResponseConfig(PATH_TO_HTTP, DEFAULT_FILES, NOT_FOUND_FILE)
+    return ResponseConfig(PATH_TO_HTTP, DEFAULT_FILES, NOT_FOUND_FILE, INTERNAL_ERROR_FILE)
--- server_files_body.py
+++ server_files_body.py
@@ -15,10 +15,10 @@ def verify_path(func):
         requested_path = os.path.abspath(os.path.join(base_path, file_path))
 
         if not requested_path.startswith(base_path):
-            return get_not_found_body()
+            return get_internal_error_body()
 
         if not VALID_FILENAME_REGEX.match(os.path.basename(requested_path)):
-            return get_not_found_body()
+            return get_internal_error_body()
 
         return func(file_path, *args, **kwargs)
     return wrapper
@@ -50,3 +50,12 @@ def get_specific_file_body(file_path):
         return file_body
     else:
         return get_not_found_body()
+
+def get_internal_error_body():
+    path = "./" + config.path_to_http_dir + config.internal_error_file
+    if os.path.isfile(path):
+        with open(path, 'r', encoding="utf-8") as f:
+            file_body = f.read()
+        return file_body
+    else:
+        return "<h1>500 Internal Server Error</h1>"
