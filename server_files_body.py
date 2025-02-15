f3e4a40b9712637a1c13dcda0e3c53952cf8e250
marcin kossakowski <morianekxd@gmail.com>
2025-02-15 19:50:36 +0100

    added verification for user controlled path

--- server_files_body.py
+++ server_files_body.py
@@ -1,9 +1,20 @@
 import os
-from dataclasses import dataclass
+from functools import wraps
 
 from config import get_response_config
 
 config = get_response_config()
+
+def verify_path(func):
+    @wraps(func)
+    def wrapper(file_path, *args, **kwargs):
+        base_path = os.path.abspath("./" + config.path_to_http_dir)
+        requested_path = os.path.abspath(os.path.join(base_path, file_path))
+
+        if not requested_path.startswith(base_path):
+            return get_not_found_body()
+        return func(file_path, *args, **kwargs)
+    return wrapper
 
 def get_default_file_body():
     for file in config.default_files:
@@ -23,6 +34,7 @@ def get_not_found_body():
     else:
         return "<h1>404 Not found</h1>"
 
+@verify_path
 def get_specific_file_body(file_path):
     path = "./" + config.path_to_http_dir + file_path
     if os.path.isfile(path):
