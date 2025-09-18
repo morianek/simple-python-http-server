ed45a763e6bfd486ae0b3c0a0e39e60fdf99b3e9
marcin kossakowski <morianekxd@gmail.com>
2025-09-18 19:01:00 +0200

    changed a test

--- tests/client_test.py
+++ tests/client_test.py
@@ -6,6 +6,11 @@ host = '127.0.0.1'
 port = 8888
 
 s.connect((host, port))
+
+TEST_REQUEST = b'''GET /test?param1=value1&param2=value2 HTTP/1.1\r
+'''
+
+s.send(TEST_REQUEST)
 print(str(s.recv(1024)))
 
 s.close()
